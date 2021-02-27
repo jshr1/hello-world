173fddf31567fdbee66e7467e4381a1299871867
jshr1 <79728985+jshr1@users.noreply.github.com>
2021-02-26 18:21:30 -0700

    Update GitDemo.java
    The code for findFactorial and reverseNumber were swapped

--- GitDemo.java
+++ GitDemo.java
@@ -4,7 +4,7 @@
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -23,6 +23,30 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
+        /*double factorialNumber = 0;
+        
+        while( num != 0 ) {
+            double temp = num % 10;
+            factorialNumber = factorialNumber * 10 + temp;
+            num /= 10;
+        }
+        
+        return factorialNumber;*/
+        double newNumber = 1;
+        
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
+        }
+        reverseNum=newNumber;
+        return newNumber;
+    }
+    
+    /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+    public double reverseNumber(int num){
         double factorialNumber = 0;
         
         while( num != 0 ) {
@@ -32,21 +56,13 @@ public class GitDemo {
         }
         
         return factorialNumber;
-    }
-    
-    /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
-        double newNumber = 1;
+        /*double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
-        return newNumber;
+        reverseNum=newNumber;
+        return newNumber;*/
     }
     
 }
